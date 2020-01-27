4e1137340b162b3d078ac0fa77715d231d99e983
Christian <cristiplesca1993@gmail.com>
2020-01-27 14:20:02 -0800

    wireframes added comment pannel restyled

--- static/js/main.js
+++ static/js/main.js
@@ -55,17 +55,7 @@
 });
 
 
-/*==================================================================
-    [ Textarea ]*/
-var text_max = 200;
-$('#count_message').html('0 / ' + text_max );
 
-$('#text').keyup(function() {
-  var text_length = $('#text').val().length;
-  var text_remaining = text_max - text_length;
-  
-  $('#count_message').html(text_length + ' / ' + text_max);
-});
 
 
 
